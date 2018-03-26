0cdd85476067e9a2583d7f08b18dbdc1e05abff7
mevanstron <michael@michaelgregoryevans.com>
2018-03-26 16:48:03 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,19 @@
-const code = [65, 65, 65, 65, 65, 65, 65, 65, 65, 65]
-const code1 = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code1 = [65, 65, 65, 65, 65, 65, 65, 65, 65, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
 
   function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
+    const key = parseInt(e.location || e.detail || e.which);
     console.log(key)
     if (key === code[index]) {
         console.log(index)
         index++;
 
         if (index === code.length) {
+          alert("Hurray!")
           console.log("Hurray!");
           index = 0;
       }
